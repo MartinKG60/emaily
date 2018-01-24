7e5925f926fced30b33a277648aa866414b99a65
Martin Gammelholm <mag@dis-play.dk>
2018-01-24 10:45:54 +0100

    Changed greeting

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send({
-        hi: 'there'
+        hello: 'world'
     });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// Heroku URL
+// https://agile-springs-67245.herokuapp.com/
